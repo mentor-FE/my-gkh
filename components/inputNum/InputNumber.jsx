92bb01943cbfb0beefb26017c9fd58023ac70e7b
CheeseCheesson <kira7999@gmail.com>
2023-10-02 12:51:28 +0300

    fix

--- components/inputNum/InputNumber.jsx
+++ components/inputNum/InputNumber.jsx
@@ -6,8 +6,8 @@ const InputNumber = ({onChange, name, id, text }) => {
         type='number'
         name={name}
         id={id}
-        className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-        placeholder=' '
+        className='placeholder:pl-40 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+        placeholder='00,00'
         step='0.01'
         min='0'
         required
